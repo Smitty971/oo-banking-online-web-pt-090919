5b3f817348878e7173cbf1165ac4907c4c94d152
Jamal Turner <jlsmitty2273@yahoo.com>
2019-10-12 17:47:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,34 +1,20 @@
 class Transfer
-  attr_accessor :sender, :receiver, :status, :amount
+  attr_reader :amount, :sender, :receiver
+  attr_accessor :status
 
+  def initialize(sender, receiver, amount)
+    @status = "pending"
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+  end
 
-   def initialize(sender, receiver, amount)
-     @sender = sender
-     @receiver = receiver
-     @amount = amount
-     self.status = "pending"
-     self.amount = 50
-   end
+  def valid?
+    sender.valid? && receiver.valid?
+  end
 
-   def valid?
-     #self.status == self.status
-     #self.status.valid?
-     self.sender.valid? && self.receiver.valid?
-   end
-
-   def execute_transaction
-    #if
-    #  !self.valid? || self.sender.balance < self.amount
-    #  self.status = "rejected"
-    #  "Transaction rejected. Please check your account balance."
-    #elsif
-    #  status == "pending"
-    #  self.sender.balance -= self.amount
-    #  self.receiver.balance += self.amount
-    #  self.status = "complete"
-    #end
-   #end
-   if valid? && sender.balance > amount && self.status == "pending"
+  def execute_transaction
+    if valid? && sender.balance > amount && self.status == "pending"
       sender.balance -= amount
       receiver.balance += amount
       self.status = "complete"
@@ -37,18 +23,18 @@ class Transfer
     end
   end
 
-   def reverse_transfer
-     if self.status == "complete"
-       self.receiver.balance -= self.amount
-       self.sender.balance += self.amount
-       self.status = "reversed"
-     end
-   end
-   def reject_transfer
-     self.status = "rejected"
-     "Transaction rejected. Please check your account balance."
-   end
- end
+  def reverse_transfer
+    if valid? && receiver.balance > amount && self.status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
+      self.status = "reversed"
+    else
+      reject_transfer
+    end
+  end
 
-
+  def reject_transfer
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
 end
